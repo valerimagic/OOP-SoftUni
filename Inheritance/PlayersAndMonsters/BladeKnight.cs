1332e8c114982959fe3d5a0e68e63cdeded6e4f3
Valeri Hristov <valerimagic@gmail.com>
2020-03-28 23:30:55 +0200

    new

---
+++ Inheritance/PlayersAndMonsters/BladeKnight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class BladeKnight : DarkKnight
+    {
+        public BladeKnight(string username, int level) 
+            : base(username, level)
+        {
+        }
+
+        public string Username { get; set; }
+
+        public int Level { get; set; }
+    }
+}
