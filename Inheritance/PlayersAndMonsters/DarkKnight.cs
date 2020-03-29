08daade38d8da7bff239786c0a684981d82012c1
Valeri Hristov <valerimagic@gmail.com>
2020-03-29 21:26:40 +0300

    new

---
+++ Inheritance/PlayersAndMonsters/DarkKnight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class DarkKnight : Knight
+    {
+        public DarkKnight(string username, int level)
+            : base(username, level)
+        {
+        }
+        public string Username { get; set; }
+        public int Level { get; set; }
+
+    }
+
+
+}
