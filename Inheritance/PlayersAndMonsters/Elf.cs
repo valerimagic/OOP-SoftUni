aa797b79f83252771f482eef72f04fac2c9e5368
Valeri Hristov <valerimagic@gmail.com>
2020-03-31 23:53:14 +0300

    new

---
+++ Inheritance/PlayersAndMonsters/Elf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Elf : Hero
+    {
+        public Elf(string username, int level) : base(username, level)
+        {
+
+        }
+
+        public string Username { get; set; }
+
+        public int Level { get; set; }
+
+    }
+}
