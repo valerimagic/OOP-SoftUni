de3c70f6b8bbd0e412c4485fdc2ec6f055f49e4e
Valeri Hristov <valerimagic@gmail.com>
2020-04-02 19:48:05 +0300

    new

---
+++ Encapsulation/Encapsulation/Encapsulation/Box.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasBoxData
+{
+    public class Box
+    {
+        private const double sideMinValue = 0;
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+
+            set
+            {
+                if (value <= sideMinValue)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value <= sideMinValue)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                if (value <= sideMinValue)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+
+            double surfaceArea = (2 * this.Length * this.Width) + (2 * this.Length * this.Height) + (2 * this.Width * this.Height);
+
+            return surfaceArea;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            double lateralSurfaceArea = (2 * this.Length * this.Height) + (2 * this.Width * this.Height);
+
+            return lateralSurfaceArea;
+        }
+
+        public double Volume()
+        {
+            double volume = this.Length * this.Height * this.Width;
+
+            return volume;
+        }
+    }
+}
