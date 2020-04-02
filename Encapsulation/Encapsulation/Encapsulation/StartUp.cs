c7ec3d84361dee78811d5201f2472ae938d902c2
Valeri Hristov <valerimagic@gmail.com>
2020-04-02 19:49:47 +0300

    new

---
+++ Encapsulation/Encapsulation/Encapsulation/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using ClasBoxData;
+using System;
+
+namespace Encapsulation
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var lengthA = double.Parse(Console.ReadLine());
+            var widthA = double.Parse(Console.ReadLine());
+            var heightA = double.Parse(Console.ReadLine());
+            try
+            {
+                Box box = new Box(lengthA, widthA, heightA);
+
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+
+            }
+
+            catch(ArgumentException message)
+            {
+                Console.WriteLine(message.Message);
+            }
+
+        }
+    }
+}
