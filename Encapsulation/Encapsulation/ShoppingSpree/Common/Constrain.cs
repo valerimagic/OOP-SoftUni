f4d4070097e9e22610acfc89f820218cad60d30e
Valeri Hristov <valerimagic@gmail.com>
2020-04-03 21:29:27 +0300

    new

---
+++ Encapsulation/Encapsulation/ShoppingSpree/Common/Constrain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree.Common
+{
+    public static class Constrain
+    {
+        public static string nameCannotBeEmpty = "Name cannot be empty";
+
+        public static string moneyCannotBeNegative = "Money cannot be negative";
+
+    }
+}
