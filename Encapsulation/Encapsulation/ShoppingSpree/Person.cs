8bd4b352f13fe92c3161a91cd4d2763c63be4e2d
Valeri Hristov <valerimagic@gmail.com>
2020-04-05 22:12:23 +0300

    update

--- Encapsulation/Encapsulation/ShoppingSpree/Person.cs
+++ Encapsulation/Encapsulation/ShoppingSpree/Person.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ShoppingSpree.Common;
+using ShoppingSpree.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,68 @@ namespace ShoppingSpree
 {
     class Person
     {
+        private string name;
+        private decimal money;
+        private List<Product> bag;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bag = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(Constrain.nameCannotBeEmpty);
+                }
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException(Constrain.moneyCannotBeNegative);
+                }
+                this.money = value;
+            }
+        }
+
+        public void Buy(Product product)
+        {
+            if(product.Cost > this.money)
+            {
+                throw new InvalidOperationException($"{this.Name} can't afford {product.Name}");
+            }
+            this.bag.Add(product);
+            this.money -= product.Cost;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+
+
     }
 }
