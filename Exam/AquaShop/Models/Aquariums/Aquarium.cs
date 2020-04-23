5f3f78853e76f2a594c63f3c277e3316369c9d06
Valeri Hristov <valerimagic@gmail.com>
2020-04-23 23:53:26 +0300

    new

---
+++ Exam/AquaShop/Models/Aquariums/Aquarium.cs
@@ -0,0 +1,84 @@
+﻿using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish.Contracts;
+using AquaShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Models.Aquariums
+{
+    public abstract class Aquarium : IAquarium
+    {
+        private string name;
+        private int comfort;
+
+        private readonly List<IDecoration> decorations;
+        private readonly List<IFish> fish;
+
+        public Aquarium(string name, int capacity)
+        {
+            this.decorations = new List<IDecoration>();
+            this.fish = new List<IFish>();
+        }
+
+        public string Name
+        {
+            get => this.name; 
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidAquariumName);
+                }
+                this.name = value;
+            }
+        }
+        public int Capacity { get; }
+
+        public int Comfort => this.Decorations.Sum(c => c.Comfort);
+
+
+        public ICollection<IDecoration> Decorations => this.decorations.AsReadOnly();
+
+        public ICollection<IFish> Fish => this.fish.AsReadOnly();
+
+        public void AddDecoration(IDecoration decoration)
+        {
+            this.Decorations.Add(decoration);
+        }
+
+        public void AddFish(IFish fish)
+        {
+            //Adds a Fish in the Aquarium if there is capacity for it
+            if(this.Fish.Count == this.Capacity)
+            {
+                throw new InvalidOperationException(ExceptionMessages.NotEnoughCapacity);
+            }
+            this.Fish.Add(fish);
+        }
+
+        public void Feed()
+        {
+            foreach (var fish in this.Fish)
+            {
+                fish.Eat();
+            }
+        }
+        public bool RemoveFish(IFish fish) => this.Fish.Remove(fish);
+        public string GetInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Name} ({this.GetType().Name}):");
+            sb.AppendLine($"Fish: {(this.Fish.Any() ? string.Join(", ", this.Fish.Select(x=>x.Name)) : "none")}");
+            sb.AppendLine($"Decorations: {this.decorations.Count()}");
+            sb.AppendLine($"Comfort: {this.Comfort}");
+
+            string result = sb.ToString().TrimEnd();
+            return result;
+        }
+
+    }
+}
