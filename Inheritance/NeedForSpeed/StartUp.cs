8948107fe5b93e93bfcf84be5e7bb0f6f2a600de
Valeri Hristov <valerimagic@gmail.com>
2020-04-29 21:46:02 +0300

    new

--- Inheritance/NeedForSpeed/StartUp.cs
+++ Inheritance/NeedForSpeed/StartUp.cs
@@ -4,7 +4,13 @@
     {
         public static void Main(string[] args)
         {
-            
+
+            var car = new Car(100, 100);
+
+            car.Drive(10);
+
+            System.Console.WriteLine(car.Fuel);
+
         }
     }
 }
