d2b3c277209d2c50a33a5bc53f59db07588c8f98
Valeri Hristov <valerimagic@gmail.com>
2020-05-05 22:08:04 +0300

    new

--- Inheritance/Animals/StartUp.cs
+++ Inheritance/Animals/StartUp.cs
@@ -1,10 +1,76 @@
-﻿namespace Animals
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            List<Animal> animals = new List<Animal>();
+
+            string command;
+
+            while((command = Console.ReadLine()) != "Beast")
+            {
+                string[] input = Console.ReadLine().Split(" ");
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                string gender = input[2];
+
+                try
+                {
+                    if(command == "Cat")
+                    {
+                        animals.Add(new Cat(name, age, gender));
+                    }
+                    else if(command == "Dog")
+                    {
+                        animals.Add(new Dog(name, age, gender));
+                    }
+                    else if (command == "Frog")
+                    {
+                        animals.Add(new Frog(name, age, gender));
+                    }
+                    else if (command == "Dog")
+                    {
+                        animals.Add(new Dog(name, age, gender));
+                    }
+                    else if (command == "Kitten")
+                    {
+                        animals.Add(new Kitten(name, age, gender));
+                    }
+
+                    else
+                    {
+                        throw new ArgumentException("Invalid input!");
+                    }
+
+
+
+
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+
+            }
+
+            animals.ForEach(Console.WriteLine);
+
+        }
+
+        private void PrintAnimals(List<Animal> animals)
+        {
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine(animal.ToString().TrimEnd());
+            }
         }
     }
 }
