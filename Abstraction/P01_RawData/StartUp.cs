cd9c9c84b2eba53cc4178f0344385bf01cf37972
Valeri Hristov <valerimagic@gmail.com>
2020-05-20 22:28:35 +0300

    new

---
+++ Abstraction/P01_RawData/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using P01_RawData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace StartUp
+{
+    public class StartUp
+    {
+
+        public static void Main(string[] args)
+        {
+            RunEngine runEngine = new RunEngine();
+            runEngine.Run();
+            
+
+           
+        }
+
+    }
+    
+
+}
