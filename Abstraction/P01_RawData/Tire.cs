7ef9357a6e93342900cb4789ac1a598fd142cc67
Valeri Hristov <valerimagic@gmail.com>
2020-05-20 22:28:52 +0300

    new

---
+++ Abstraction/P01_RawData/Tire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StartUp
+
+{
+    public class Tire
+    {
+        public Tire(double pressure, int age)
+        {
+            this.Pressure = pressure;
+            this.Age = age;
+        }
+
+        public double Pressure { get; set; }
+
+        public int Age { get; set; }
+    }
+}
