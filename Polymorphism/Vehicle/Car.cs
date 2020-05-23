a9d4436da833ffb4a1efa2bbcf80894c559356b0
Valeri Hristov <valerimagic@gmail.com>
2020-05-23 23:57:54 +0300

    new

---
+++ Polymorphism/Vehicle/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicle
+{
+    public class Car
+    {
+
+
+
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+
+        public void CarTravell(int distance)
+        {
+
+            if(this.FuelConsumption * distance < this.FuelQuantity)
+            {
+                Console.WriteLine("Car need refueling");
+            }
+
+            this.FuelQuantity -= distance * this.FuelConsumption;
+        }
+
+    }
+}
