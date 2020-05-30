8eac8bfaea77e36734afb34623c7da5505f60db6
Valeri Hristov <valerimagic@gmail.com>
2020-05-30 22:50:42 +0300

    update

--- Polymorphism/Vehicle/Truck.cs
+++ Polymorphism/Vehicle/Truck.cs
@@ -6,14 +6,15 @@ namespace Exe1Vehicle
 {
     public class Truck : Vehicle
     {
-        public Truck(double fuelQuantity, double fuelConsumption) 
-            : base(fuelQuantity, fuelConsumption + 1.6)
+        public Truck(double fuelQuantity, double fuelConsumption, double tankCapacity) 
+            : base(fuelQuantity, fuelConsumption + 1.6, tankCapacity)
         {
         }
 
         public override void Refuel(double liters)
         {
-            base.Refuel(liters * 0.95);
+            base.Refuel(liters);
+            this.fuelQuantity -= liters * 0.05;
         }
     }
 }
