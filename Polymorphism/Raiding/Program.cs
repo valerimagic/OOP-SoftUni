5acd5f4442812614a58612b46a45d328aa08e67e
Valeri Hristov <valerimagic@gmail.com>
2020-05-31 21:37:13 +0300

    new

---
+++ Polymorphism/Raiding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Raiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
