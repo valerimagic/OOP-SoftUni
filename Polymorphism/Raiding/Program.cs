c2cac89ebbb59f56c1e03db5e6cfea9afa33d9ca
Valeri Hristov <valerimagic@gmail.com>
2020-06-04 23:54:37 +0300

    new

--- Polymorphism/Raiding/Program.cs
+++ Polymorphism/Raiding/Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Raiding
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+            List<BaseHero> baseHero = new List<BaseHero>();
+
+            for (int i = 0; i < number; i++)
+            {
+                var name = Console.ReadLine();
+                var theCode = Console.ReadLine();
+                
+                if(theCode == "Paladin")
+                {
+                    var paladin = new Paladin(name);
+                    baseHero.Add(paladin);
+                }
+
+                else if(theCode == "Druid")
+                {
+                    var druid = new Druid(name);
+                    baseHero.Add(druid);
+                }
+
+                else if(theCode == "Warrior")
+                {
+                    var warrior = new Warrior(name);
+                    baseHero.Add(warrior);
+                    
+                }
+
+                else if(theCode == "Rogue")
+                {
+                    var rogue = new Rogue(name);
+                    baseHero.Add(rogue);
+                }
+
+                else
+                {
+                    i--;
+                    Console.WriteLine("Invalid hero!");
+                }
+
+
+            }
+
+            int pointTheBoss = int.Parse(Console.ReadLine());
+
+                int sum = 0;
+            foreach (var item in baseHero)
+            {
+                int currentSum = item.Power;
+                sum += currentSum;
+                currentSum = 0;
+                Console.WriteLine(item.CastAbility());
+            }
+
+            if(sum >= pointTheBoss)
+            {
+                Console.WriteLine("Victory!");
+            }
+
+            else
+            {
+                Console.WriteLine("Defeat...");
+            }
+
         }
     }
 }
