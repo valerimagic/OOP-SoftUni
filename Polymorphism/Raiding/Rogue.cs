fa60100860e7f8fd86fb87fe9ce2dce743c73b46
Valeri Hristov <valerimagic@gmail.com>
2020-06-08 23:02:15 +0300

    new

---
+++ Polymorphism/Raiding/Rogue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Rogue : BaseHero
+    {
+        private const int roguePower = 80;
+        public Rogue(string name) : base(name)
+        {
+            this.Power = roguePower;
+        }
+
+        public override string CastAbility()
+        {
+            return $"Rogue - {this.Name} hit for {this.Power} damage";
+        }
+    }
+}
