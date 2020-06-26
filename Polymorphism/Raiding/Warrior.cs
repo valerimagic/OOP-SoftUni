cb31a31e708c4da2184cdeee5e9b458b58d6f00e
Valeri Hristov <valerimagic@gmail.com>
2020-06-26 22:10:12 +0300

    new

---
+++ Polymorphism/Raiding/Warrior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Warrior : BaseHero
+    {
+        private const int warriorPower = 100;
+        public Warrior(string name) : base(name)
+        {
+            this.Power = warriorPower;
+        }
+
+        public override string CastAbility()
+        {
+            return $"Warrior - {this.Name} hit for {this.Power} damage";
+        }
+    }
+}
