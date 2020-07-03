2f492a814be5f8c80a217775341a8065d417d109
Valeri Hristov <valerimagic@gmail.com>
2020-07-03 23:28:30 +0300

    new

---
+++ Polymorphism/Raiding/Paladin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Paladin : BaseHero
+    {
+        private const int powerPaladin = 100;
+        public Paladin(string name) : base(name)
+        {
+            this.Power = powerPaladin;
+        }
+
+        public override string CastAbility()
+        {
+            return $"Paladin - {this.Name} healed for {this.Power}";
+        }
+    }
+}
