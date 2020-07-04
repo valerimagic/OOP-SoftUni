26c76292f93b705273a1b32220f53cee14572167
Valeri Hristov <valerimagic@gmail.com>
2020-07-04 23:09:13 +0300

    new

---
+++ Polymorphism/Raiding/Druid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Druid : BaseHero
+    {
+        private const int powerDruid = 80;
+
+        public Druid(string name) 
+            : base(name)
+        {
+            this.Power = powerDruid;
+        }
+
+        public override string CastAbility()
+        {
+            return $"Druid - {this.Name} healed for {this.Power}";
+        }
+    }
+}
