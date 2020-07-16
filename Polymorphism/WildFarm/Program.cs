3e09f4c16a421a50eff53022d5748a49854ce724
Valeri Hristov <valerimagic@gmail.com>
2020-07-16 23:10:03 +0300

    new

--- Polymorphism/WildFarm/Program.cs
+++ Polymorphism/WildFarm/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using WildFarm.Core;
+using WildFarm.Core.Contracts;
 
 namespace WildFarm
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
+            IEngine engine = new Engine();
+            engine.Run();
         }
     }
 }
