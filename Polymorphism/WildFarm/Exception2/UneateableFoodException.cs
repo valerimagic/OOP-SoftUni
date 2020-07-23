8ee1b3fe83ff39eb3ffad1a41bd4eb4d7301e708
Valeri Hristov <valerimagic@gmail.com>
2020-07-23 22:25:01 +0300

    new

---
+++ Polymorphism/WildFarm/Exception2/UneateableFoodException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm.Exception2
+
+{
+    public class UneateableFoodException : Exception
+    {
+
+        private const string Def_Exc_Msg = "{AnimalType} does not eat {FoodType}!";
+        public UneateableFoodException()
+        {
+        }
+
+        public UneateableFoodException(string message) : base(message)
+        {
+        }
+    }
+}
