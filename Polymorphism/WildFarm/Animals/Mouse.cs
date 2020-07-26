a41fcba1e467c2e0f262d55b68d6c7b83f31eae0
Valeri Hristov <valerimagic@gmail.com>
2020-07-26 23:34:29 +0300

    new

---
+++ Polymorphism/WildFarm/Animals/Mouse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WildFarm.Foods;
+
+namespace WildFarm.Animals
+{
+    public class Mouse : Mammal
+    {
+        private const double WEIGHT_MULTIPLIER = 0.10;
+        public Mouse(string name, double weight, string livingRegion) : base(name, weight, livingRegion)
+        {
+        }
+
+        public override ICollection<Type> PrefferedFoods => new List<Type>() { typeof(Vegetable), typeof(Fruit) };
+
+        public override double WeightMultiplier => WEIGHT_MULTIPLIER;
+
+        public override string ProduceSound()
+        {
+            return $"Squeak";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" {this.Weight}, {this.LivingRegion}, {this.FoodEaten}]";
+        }
+    }
+}
