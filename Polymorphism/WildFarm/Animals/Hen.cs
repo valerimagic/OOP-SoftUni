fe6467e9582f9be8ae1fbabb2fc3a4715e030eb1
Valeri Hristov <valerimagic@gmail.com>
2020-07-29 22:47:05 +0300

    new

---
+++ Polymorphism/WildFarm/Animals/Hen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WildFarm.Foods;
+
+namespace WildFarm.Animals
+{
+    public class Hen : Bird
+    {
+
+        private const double WEIGHT_MULTIPLIER = 0.35;
+
+        public Hen(string name, double weight, double wingSize) : base(name, weight, wingSize)
+        {
+
+        }
+
+        public override ICollection<Type> PrefferedFoods => new List<Type>() { typeof(Vegetable), typeof(Fruit), typeof(Meat), typeof(Seeds) };
+
+        public override double WeightMultiplier => WEIGHT_MULTIPLIER;
+
+
+        public override string ProduceSound()
+        {
+            return $"Cluck";
+        }
+    }
+}
