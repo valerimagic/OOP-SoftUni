4cad1be128444d86d9020d0b4296ae9c112122a5
Valeri Hristov <valerimagic@gmail.com>
2020-08-01 21:57:11 +0300

    new

---
+++ Polymorphism/WildFarm/Animals/Cat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WildFarm.Foods;
+
+namespace WildFarm.Animals
+{
+    public class Cat : Feline
+    {
+        private const double WEIGHT_MULTIPLIER = 0.30;
+        public Cat(string name, double weight, string livingRegion, string breed) : base(name, weight, livingRegion, breed)
+        {
+        }
+
+        public override ICollection<Type> PrefferedFoods => new List<Type>() { typeof(Vegetable), typeof(Meat) };
+
+        public override double WeightMultiplier => WEIGHT_MULTIPLIER;
+
+        public override string ProduceSound()
+        {
+            return $"Meow";
+        }
+    }
+}
