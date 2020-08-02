2d88e4127b71792cbb6d8287a558fa284bedc86e
Valeri Hristov <valerimagic@gmail.com>
2020-08-02 21:26:27 +0300

    new

---
+++ Polymorphism/WildFarm/Animals/Bird.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WildFarm.Foods.Contracts;
+
+namespace WildFarm.Animals
+{
+    public abstract class Bird : Animal
+    {
+        protected Bird(string name, double weight, double wingSize) 
+            : base(name, weight)
+        {
+            this.WingSize = wingSize;
+        }
+
+        public double WingSize { get; private set; }
+
+
+        public override string ToString()
+        {
+            return base.ToString() + $" {this.WingSize}, {this.Weight}, {this.FoodEaten}]";
+        }
+    }
+}
