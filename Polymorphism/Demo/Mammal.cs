ddb6335fadc1165459243b103406715e44b06e03
Valeri Hristov <valerimagic@gmail.com>
2020-08-08 22:04:57 +0300

    new

---
+++ Polymorphism/Demo/Mammal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    public abstract class Mammal
+    {
+
+        public void Sleep()
+        {
+
+        }
+    }
+}
