c75d68fa27ca535ddb1efabc73a13c00f8968951
Valeri Hristov <valerimagic@gmail.com>
2020-08-08 22:04:33 +0300

    new

---
+++ Polymorphism/Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Mammal mammal = new Cat();
+            IAnimal animal = new Cat();
+            Console.WriteLine(cat.MiceKilled);
+
+
+            
+
+
+
+
+
+
+
+        }
+    }
+}
