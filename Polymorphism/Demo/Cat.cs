e5582fd8e5daaf3b7c0ac1b52b1804b9a4e5e28d
Valeri Hristov <valerimagic@gmail.com>
2020-08-10 22:43:22 +0300

    new

---
+++ Polymorphism/Demo/Cat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    class Cat : Mammal, IAnimal
+    {
+        public IEnumerable<IAnimal> Children { get; } = new List<IAnimal>();
+
+        public string SayHello()
+        {
+            return "Zdr!";
+        }
+
+        public string Valeri()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MiceKilled { get; private set; } = 10000;
+
+        
+    }
+}
