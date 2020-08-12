450475102dc4b3c5e339b30c35a17afae5117cc7
Valeri Hristov <valerimagic@gmail.com>
2020-08-12 22:09:57 +0300

    new

---
+++ Exam/AquaShop/Models/Fish/FreshwaterFish.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Fish
+{
+    public class FreshwaterFish : Fish
+    {
+        private const int FishInitialSize = 3;
+        public FreshwaterFish(string name, string species, decimal price) : base(name, species, price)
+        {
+            this.Size = FishInitialSize;
+        }
+
+        public override void Eat()
+        {
+            this.Size += 3;
+        }
+    }
+}
