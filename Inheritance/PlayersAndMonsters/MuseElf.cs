fa221f7c09f2bf0301d48a59244f65af07e44e6e
Valeri Hristov <valerimagic@gmail.com>
2020-08-13 21:22:33 +0300

    new

---
+++ Inheritance/PlayersAndMonsters/MuseElf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class MuseElf : Elf
+    {
+        public MuseElf(string username, int level) 
+            : base(username, level)
+        {
+
+        }
+
+        public string Username { get; set; }
+
+        public int Level { get; set; }
+
+    }
+}
