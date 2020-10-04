e643398f1598a55f4b0583649d41444bba9bca8c
Valeri Hristov <valerimagic@gmail.com>
2020-10-04 22:38:55 +0300

    new

---
+++ Encapsulation/Encapsulation/Problem1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Problem1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+
+            try
+            {
+                var box = new Box(a, b, c);
+                Console.WriteLine($"Surface Area - {box.GetSurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.GetLateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.GetVolume():F2}");
+
+            }
+            catch (ArgumentException messages)
+            {
+
+                Console.WriteLine(messages.Message);
+            }
+
+
+
+        }
+    }
+}
