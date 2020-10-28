6fe2ccaf604e4fb5e3fbe7bf881ce0ae6b6b1d3a
Valeri Hristov <valerimagic@gmail.com>
2020-10-28 22:31:55 +0200

    new

---
+++ Exam/CounterStriker/CounterStrike/Models/Maps/Map.cs
@@ -0,0 +1,73 @@
+﻿using CounterStrike.Models.Maps.Contracts;
+using CounterStrike.Models.Players;
+using CounterStrike.Models.Players.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CounterStrike.Models.Maps
+{
+    public class Map : IMap
+    {
+        public string Start(ICollection<IPlayer> players)
+        {
+            var terrorists = players.Where(x => x.GetType() == typeof(Terrorist)).ToList();
+            var counterTerrorists = players.Where(x => x.GetType() == typeof(CounterTerrorist)).ToList();
+
+            while (terrorists.Any(x => x.IsAlive)
+            && counterTerrorists.Any(x => x.IsAlive))
+            {
+
+                //foreach (var item in players)
+                //{ 
+                //    if(item.IsAlive == true)
+                //    {
+
+                //    }
+                //}
+
+
+                foreach (var terro in terrorists)
+                {
+                    if (!terro.IsAlive)
+                    {
+                        continue;
+                    }
+                    foreach (var counterTerro in counterTerrorists)
+                    {
+                        if (!counterTerro.IsAlive)
+                        {
+                            continue;
+                        }
+
+                        counterTerro.TakeDamage(terro.Gun.Fire());
+                    }
+
+                }
+
+                foreach (var counterTerro in counterTerrorists)
+                {
+                    if (!counterTerro.IsAlive)
+                    {
+                        continue;
+                    }
+
+                    foreach (var terro in terrorists)
+                    {
+
+                        if (!terro.IsAlive)
+                        {
+                            continue;
+                        }
+                        terro.TakeDamage(counterTerro.Gun.Fire());
+                    }
+                }
+
+            }
+            return terrorists.Any(x => x.IsAlive)
+                ? "Terrorist wins!"
+                : "Counter Terrorist wins!";
+        }
+    }
+}
