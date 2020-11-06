cadc552a377992c9980654cdb02b82fa160f5c1d
Valeri Hristov <valerimagic@gmail.com>
2020-11-06 22:39:12 +0200

    new

---
+++ Exam/CounterStriker/CounterStrike/Repositories/PlayerRepository.cs
@@ -0,0 +1,28 @@
+﻿using CounterStrike.Models.Players.Contracts;
+using CounterStrike.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CounterStrike.Repositories
+{
+    public class PlayerRepository : IRepository<IPlayer>
+    {
+        public IReadOnlyCollection<IPlayer> Models => throw new NotImplementedException();
+
+        public void Add(IPlayer model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPlayer FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IPlayer model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
