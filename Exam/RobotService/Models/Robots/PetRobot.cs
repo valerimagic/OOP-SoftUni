fe0f86558ba5a9b9e5be01244832ba53326de2e5
Valeri Hristov <valerimagic@gmail.com>
2020-11-17 23:20:56 +0200

    new

---
+++ Exam/RobotService/Models/Robots/PetRobot.cs
@@ -0,0 +1,15 @@
+﻿using RobotService.Models.Robots.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotService.Models.Robots
+{
+    public class PetRobot : Robot, IRobot
+    {
+        public PetRobot(string name, int energy, int happiness, int procedureTime) 
+            : base(name, energy, happiness, procedureTime)
+        {
+        }
+    }
+}
