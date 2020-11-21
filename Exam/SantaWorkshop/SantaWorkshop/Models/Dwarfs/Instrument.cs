564cb6c33c567fc9957ea1c4b37867b25183e5a1
Valeri Hristov <valerimagic@gmail.com>
2020-11-21 21:39:08 +0200

    new

---
+++ Exam/SantaWorkshop/SantaWorkshop/Models/Dwarfs/Instrument.cs
@@ -0,0 +1,57 @@
+﻿using SantaWorkshop.Models.Instruments.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SantaWorkshop.Models.Dwarfs
+{
+    public class Instrument : IInstrument
+    {
+        private int power;
+
+        public Instrument(int power)
+        {
+            this.Power = power;
+        }
+
+        public int Power
+        {
+            get
+            {
+                return this.power;
+            }
+            private set
+            {
+                if(value < 0)
+                {
+                    this.power = 0;
+                }
+                this.power = value;
+            }
+        }
+        
+            
+            
+        
+
+        public bool IsBroken()
+        {
+            if(this.Power == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Use()
+        {
+            this.Power -= 10;
+            if(this.Power < 0)
+            {
+                this.Power = 0;
+            }
+
+        }
+    }
+}
---
+++ Exam/SantaWorkshop/SantaWorkshop/Models/Dwarfs/SleepyDwarf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SantaWorkshop.Models.Dwarfs
+{
+    public class SleepyDwarf : Dwarf
+    {
+
+        private const int initialEnergySleepyDwarf = 50;
+
+
+        public SleepyDwarf(string name) 
+            : base(name, initialEnergySleepyDwarf)
+        {
+        }
+
+        public override void Work()
+        {
+            base.Work();
+
+            this.Energy -= 5;
+        }
+    }
+}
