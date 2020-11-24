0c4f1ab9e17215fd7578900ab48688a4cc4ecc86
Valeri Hristov <valerimagic@gmail.com>
2020-11-24 23:18:58 +0200

    new

---
+++ Exam/RobotService/Models/Robots/WalkerRobot.cs
@@ -0,0 +1,15 @@
+﻿using RobotService.Models.Robots.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotService.Models.Robots
+{
+    public class WalkerRobot : Robot, IRobot
+    {
+        public WalkerRobot(string name, int energy, int happiness, int procedureTime) 
+            : base(name, energy, happiness, procedureTime)
+        {
+        }
+    }
+}
