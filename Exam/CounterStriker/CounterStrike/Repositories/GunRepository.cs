7d69a57b79863beb269487dc3b53418751438787
Valeri Hristov <valerimagic@gmail.com>
2020-11-30 22:58:21 +0200

    new

---
+++ Exam/CounterStriker/CounterStrike/Repositories/GunRepository.cs
@@ -0,0 +1,28 @@
+﻿using CounterStrike.Models.Guns.Contracts;
+using CounterStrike.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CounterStrike.Repositories
+{
+    public class GunRepository : IRepository<IGun>
+    {
+        public IReadOnlyCollection<IGun> Models => throw new NotImplementedException();
+
+        public void Add(IGun model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IGun FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IGun model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
