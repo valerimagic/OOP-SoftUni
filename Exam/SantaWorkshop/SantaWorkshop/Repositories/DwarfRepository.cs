0d2a60542acd53043416b31a590dec51faf9c9ee
Valeri Hristov <valerimagic@gmail.com>
2020-12-01 22:34:51 +0200

    new

---
+++ Exam/SantaWorkshop/SantaWorkshop/Repositories/DwarfRepository.cs
@@ -0,0 +1,35 @@
+﻿using SantaWorkshop.Models.Dwarfs.Contracts;
+using SantaWorkshop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SantaWorkshop.Repositories
+{
+    public class DwarfRepository : IRepository<IDwarf>
+
+    {
+        private readonly List<IDwarf> dwarves;
+
+        public DwarfRepository()
+        {
+            this.dwarves = new List<IDwarf>();
+        }
+
+        public IReadOnlyCollection<IDwarf> Models => this.dwarves.AsReadOnly();
+
+        public void Add(IDwarf model)
+        {
+            dwarves.Add(model);
+        }
+
+        public bool Remove(IDwarf model)
+        {
+            return dwarves.Remove(model);
+        }
+
+        public IDwarf FindByName(string name) => this.dwarves.FirstOrDefault(x => x.Name == name);
+        
+    }
+}
