3099f8bb416a843e8748c2cdbc968e8eec843d53
Valeri Hristov <valerimagic@gmail.com>
2020-12-08 22:08:41 +0200

    new

---
+++ Exam/SantaWorkshop/SantaWorkshop/Repositories/PresentRepository.cs
@@ -0,0 +1,34 @@
+﻿using SantaWorkshop.Models.Presents.Contracts;
+using SantaWorkshop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SantaWorkshop.Repositories
+{
+    public class PresentRepository : IRepository<IPresent>
+    {
+        private List<IPresent> presents;
+
+        public PresentRepository()
+        {
+            this.presents = new List<IPresent>();
+        }
+
+        public IReadOnlyCollection<IPresent> Models => this.presents.AsReadOnly();
+
+        public void Add(IPresent model)
+        {
+            this.presents.Add(model);
+        }
+
+        public IPresent FindByName(string name) => this.presents.FirstOrDefault(x => x.Name == name);
+        
+
+        public bool Remove(IPresent model)
+        {
+            return this.presents.Remove(model);
+        }
+    }
+}
