13b6e545690c9dbb173524ba250c8bec39a7e3bf
Valeri Hristov <valerimagic@gmail.com>
2020-12-13 22:34:55 +0200

    new

---
+++ Inheritance/5.Restaurant/Beverage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public abstract class Beverage : Product
+    {
+        private double milliliters;
+
+        public Beverage(string name, decimal price, double milliliters) 
+            : base(name, price)
+        {
+            this.Milliliters = milliliters;
+        }
+
+        public double Milliliters { get; internal set; }
+
+        //public string Name { get; internal set; }
+
+        //public decimal Price { get; internal set; }
+
+    }
+}
