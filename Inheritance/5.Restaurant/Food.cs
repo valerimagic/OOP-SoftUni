c2b83f468b5f12c6d03c5ab0ef8d0f797d5c29fc
Valeri Hristov <valerimagic@gmail.com>
2020-12-21 23:13:38 +0200

    new

---
+++ Inheritance/5.Restaurant/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Food : Product
+    {
+
+        public Food(string name, decimal price, double grams) 
+            : base(name, price)
+        {
+            this.Grams = grams;
+        }
+
+        //public string Name { get; internal set; }
+
+        //public decimal Price { get; internal set; }
+
+        public double Grams { get; internal set; }
+    }
+}
