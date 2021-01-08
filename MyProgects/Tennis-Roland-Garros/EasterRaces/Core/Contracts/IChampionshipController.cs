25678589d1d631e6a3481246653ef8a6f1eacb61
Valeri Hristov <valerimagic@gmail.com>
2021-01-08 10:15:24 +0200

    new

---
+++ MyProgects/Tennis-Roland-Garros/EasterRaces/Core/Contracts/IChampionshipController.cs
@@ -0,0 +1,17 @@
+﻿namespace EasterRaces.Core.Contracts
+{
+    public interface IChampionshipController
+    {
+        string CreatePlayer(string name, int age, string country, string city, int numberOfPlayedMatchs, int win, int loss);
+
+        string CreateCar(string type, string model, int horsePower);
+
+        string CreateTournament(string name, int maxCountPlayer);
+
+        string AddDriverToRace(string raceName, string driverName);
+
+        string AddCarToDriver(string driverName, string carModel);
+
+        string StartTournament(string tournamentName);
+    }
+}
---
+++ MyProgects/Tennis-Roland-Garros/EasterRaces/Models/Cars/Entities/Team.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Models.Drivers.Contracts;
+using EasterRaces.Utilities.Messages;
+
+namespace EasterRaces.Models.Cars.Entities
+{
+    public class Team : ITeam
+    {
+        private string nameCountry;
+        private IDictionary<string, IPlayer> teams;
+
+        public Team()
+        {
+            this.teams=new Dictionary<string, IPlayer>();
+        }
+
+        public Team(string nameCountry):this()
+        {
+            this.NameCountry = nameCountry;
+        }
+
+        public IDictionary<string, IPlayer> Teams { get; }
+
+        public string NameCountry { get; }
+        public int Goal { get; }
+        public int Loss { get; }
+        public int Draw { get; }
+        public double TotalPoints { get; }
+
+        public IPlayer Player { get; }
+
+        public void CheckAndAddPlayerToTeam(IPlayer player, string country)
+        {
+            var playerToAdd = teams.FirstOrDefault(x => x.Key == country).Value;
+
+            if (playerToAdd != null)
+            {
+                throw new ArgumentException(ExceptionMessages.PlayerExists, player.Name);
+            }
+
+            teams.Add(country, player);
+            
+        }
+        public void AddTeam(IPlayer player)
+        {
+            throw new NotImplementedException();
+        }
+        public void ResultOfTheGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
