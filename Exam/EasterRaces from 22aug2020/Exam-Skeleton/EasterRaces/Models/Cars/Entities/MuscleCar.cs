781661b9a1e8d979638f0a48031a19299c98d49c
Valeri Hristov <valerimagic@gmail.com>
2021-01-09 22:18:37 +0200

    new

---
+++ Exam/EasterRaces from 22aug2020/Exam-Skeleton/EasterRaces/Models/Cars/Entities/MuscleCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Cars.Entities
+{
+    public class MuscleCar : Car
+    {
+        private const double cubicMuscleCentimeters = 5000;
+        private const int minimumHorsePower = 400;
+        private const int maximumHorsePower = 600;
+
+        public MuscleCar(string model, int horsePower) 
+            : base(model, horsePower, cubicMuscleCentimeters, minimumHorsePower, maximumHorsePower)
+        {
+        }
+    }
+}
