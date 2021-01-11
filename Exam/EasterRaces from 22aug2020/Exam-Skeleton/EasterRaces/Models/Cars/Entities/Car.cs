7e7cbe456a6979416070c610a25a28c3786696ad
Valeri Hristov <valerimagic@gmail.com>
2021-01-11 22:24:38 +0200

    new

---
+++ Exam/EasterRaces from 22aug2020/Exam-Skeleton/EasterRaces/Models/Cars/Entities/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Utilities.Messages;
+
+namespace EasterRaces.Models.Cars.Entities
+{
+    public abstract class Car : ICar
+    {
+        private string model;
+        private int horsePower;
+        private int minHorsePower;
+        private int maxHorsePower;
+
+        public Car(string model, int horsePower, double cubicCentimeters, int minHorsePower, int maxHorsePower)
+        {
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.CubicCentimeters = cubicCentimeters;
+            this.minHorsePower = minHorsePower;
+            this.maxHorsePower = maxHorsePower;
+        }
+        public string Model
+        {
+            get => this.model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 4)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidModel, value, 4));
+                }
+
+                this.model = value;
+            }
+
+        }
+
+        public int HorsePower
+        {
+            get => this.horsePower;
+            private set
+            {
+                if (value < minHorsePower || value < maxHorsePower)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidHorsePower, value));
+                }
+            }
+        }
+        
+        public double CubicCentimeters { get; }
+        
+        public double CalculateRacePoints(int laps)
+        {
+            double result = (this.CubicCentimeters / this.HorsePower) * laps;
+
+            return result;
+        }
+    }
+}
