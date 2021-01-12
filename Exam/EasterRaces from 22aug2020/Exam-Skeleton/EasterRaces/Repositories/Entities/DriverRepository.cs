78b876b9441ddaa45e69aa56f811e3bbb179edc7
Valeri Hristov <valerimagic@gmail.com>
2021-01-12 23:15:49 +0200

    new

---
+++ Exam/EasterRaces from 22aug2020/Exam-Skeleton/EasterRaces/Repositories/Entities/DriverRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EasterRaces.Models.Drivers.Contracts;
+using EasterRaces.Repositories.Contracts;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class DriverRepository : Repository<IDriver>
+    {
+        public override IDriver GetByName(string name)
+        {
+            var driver = base.Models.FirstOrDefault(x => x.Name == name);
+            return driver;
+        }
+    }
+}
