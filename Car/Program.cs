e023ee88c8e10c10c64e9e30872f7ea2beaf9dfc
Valeri Hristov <valerimagic@gmail.com>
2021-02-03 23:07:09 +0200

    new

---
+++ Car/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CarParking
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Parking parking = new Parking();
+
+            for (int i = 0; i < 5; i++)
+            {
+                parking.AddCar(new Car("AlfaRomeo"));
+                //parking.Cars.Add(new Car("Volvo"));
+            }
+            foreach (var car in parking.Cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+
+
+        }
+    }
+}
