59ee8c788096e97197923a7cec457daf95147d1f
Valeri Hristov <valerimagic@gmail.com>
2021-02-05 22:40:03 +0200

    new

---
+++ MyProjects/Football/WorldFootball/IO/ConsoleReader.cs
@@ -0,0 +1,14 @@
+﻿namespace Football.IO
+{
+    using System;
+
+    using Contracts;
+
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
