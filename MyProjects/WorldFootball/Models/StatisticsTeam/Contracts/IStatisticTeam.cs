2d96ee6d1c3c4eb5020c1b282128bc46641a8823
Valeri Hristov <valerimagic@gmail.com>
2021-02-07 22:44:51 +0200

    new

---
+++ MyProjects/WorldFootball/Models/StatisticsTeam/Contracts/IStatisticTeam.cs
@@ -0,0 +1,17 @@
+﻿using Football.Models.StatisticsTeam.Contracts;
+
+namespace Football.Models.StatisticsTeam.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface IStatisticTeam
+    {
+        string Name { get; }
+
+        int Laps { get; }
+
+        //IReadOnlyCollection<IDriver> Drivers { get; }
+
+        //void AddDriver(IDriver driver);
+    }
+}
