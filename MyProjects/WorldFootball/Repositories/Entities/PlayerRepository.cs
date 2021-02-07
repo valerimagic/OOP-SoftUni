3763e2a29c7393fbc0949c30797fab4ba5f61192
Valeri Hristov <valerimagic@gmail.com>
2021-02-07 22:43:47 +0200

    new

---
+++ MyProjects/WorldFootball/Repositories/Entities/PlayerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Football.Models.Players.Contracts;
+using Football.Repositories.Contracts;
+
+namespace Football.Repositories.Entities
+{
+    public class PlayerRepository //: Repository<IPlayer>
+    {
+        //public override IDriver GetByName(string name)
+        //{
+        //    var driver = base.Models.FirstOrDefault(x => x.Name == name);
+        //    return driver;
+        //}
+    }
+}
---
+++ MyProjects/WorldFootball/Repositories/Entities/RaceRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Football.Repositories.Entities
+{
+    public class RaceRepository //: Repository<IRace>
+    {
+        //public override IRace GetByName(string name)
+        //{
+        //    var race = base.Models.FirstOrDefault(x => x.Name == name);
+        //    return race;
+        //}
+    }
+}
