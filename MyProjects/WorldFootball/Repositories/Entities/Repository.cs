a3af60501ec228b228f58a43da060412c712cfff
Valeri Hristov <valerimagic@gmail.com>
2021-02-18 23:35:07 +0200

    update

--- MyProjects/WorldFootball/Repositories/Entities/Repository.cs
+++ MyProjects/WorldFootball/Repositories/Entities/Repository.cs
@@ -8,35 +8,7 @@ namespace Football.Repositories.Entities
 {
     public abstract class Repository<T> //: IRepository<T>
     {
-        //private List<T> models;
-
-
-        protected Repository()
-        {
-            //this.models = new List<T>();
-        }
-
-
-        //public IReadOnlyCollection<T> Models => (IReadOnlyCollection<T>)this.models;
-
-
-        //public void Add(T model)
-        //{
-        //    this.models.Add(model);
-        //}
-
-        //public bool Remove(T model)
-        //{
-        //    return this.models.Remove(model);
-        //}
-
-        //public abstract T GetByName(string name);
-
-
-        //public IReadOnlyCollection<T> GetAll()
-        //{
-        //    return this.Models;
-        //}
+        
 
     }
 }
