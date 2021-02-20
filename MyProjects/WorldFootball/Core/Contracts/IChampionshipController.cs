594fe316d90b419fa927d9d56d1892d07cc54741
Valeri Hristov <valerimagic@gmail.com>
2021-02-20 16:21:30 +0200

    update

---
+++ MyProjects/WorldFootball/Core/Contracts/IChampionshipController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Football.Models.Players.Contracts;
+using Football.Models.Players.Entities;
+
+namespace Football.Core.Contracts
+{
+    public interface IChampionshipController
+    {
+        string CreatePlayer(string name, int age, string country, string city, int idTeam);
+
+        string RemovePlayer(Player player);
+
+        string CreateTeam(string teamName);
+
+    }
+}
