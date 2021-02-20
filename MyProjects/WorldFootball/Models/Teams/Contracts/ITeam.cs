a53c44b771482607806b79cb6d36840ee2f46b28
Valeri Hristov <valerimagic@gmail.com>
2021-02-20 17:08:53 +0200

    update

---
+++ MyProjects/WorldFootball/Models/Teams/Contracts/ITeam.cs
@@ -0,0 +1,17 @@
+﻿using Football.Models.Players.Contracts;
+using Football.Models.Teams.Contracts;
+
+namespace Football.Models.Teams.Contracts
+{
+    public interface ITeam
+    {
+        string NameCountry { get; }
+
+        int Goal { get; }
+
+        double TotalPoints { get; } 
+
+        IPlayer Player { get; }
+
+    }
+}
