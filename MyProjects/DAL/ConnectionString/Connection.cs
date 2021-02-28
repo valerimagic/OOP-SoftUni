952ddae82ca182dac036eef0d42661154fb43293
Valeri Hristov <valerimagic@gmail.com>
2021-02-28 22:10:28 +0200

    new

---
+++ MyProjects/DAL/ConnectionString/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.ConnectionString
+{
+    public class Connection
+    {
+
+        public const string DefaultConnection =
+            "Server=DESKTOP-Q9B2B1D\\SQLEXPRESS;Database=PhonBook;Integrated Security=True;";
+
+    }
+}
