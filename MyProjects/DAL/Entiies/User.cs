942ec7fb528097167a166ba86ed9de918d13b3f8
Valeri Hristov <valerimagic@gmail.com>
2021-02-28 22:14:13 +0200

    new

---
+++ MyProjects/DAL/Entiies/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Entiies
+{
+    public class User
+        :BaseClass
+    {
+       
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        public ICollection<Contacts> ContactID { get; set; } = new HashSet<Contacts>();
+    }
+}
