dd2aaf895df6ea2ce3faa8a29e6e1f1756314605
Valeri Hristov <valerimagic@gmail.com>
2021-02-28 22:13:34 +0200

    new

---
+++ MyProjects/DAL/DataContext/PhoneDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DAL.ConnectionString;
+using DAL.Entiies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.DataContext
+{
+    public class PhoneDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Contacts> Contactss { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Connection.DefaultConnection);
+            }
+        }
+
+    }
+}
