a2c415c4e85a72ea9c2849c8b5da86ec5b640d3c
Valeri Hristov <valerimagic@gmail.com>
2021-03-09 22:15:52 +0200

    update

--- MyProjects/WorldFootball/StartUp.cs
+++ MyProjects/WorldFootball/StartUp.cs
@@ -1,17 +1,12 @@
-﻿//using Core.Contracts;
-
-using System.Data;
-using Dataaccess.Entity;
-using Football.IO;
-using Football.IO.Contracts;
-using Football.Core.Entities;
-using Football.Core.Contracts;
-using Football.Models.Players.Contracts;
-using Football.Models.Players.Entities;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-
-namespace Football
+﻿namespace Football
 {
+    using Football.Core.Contracts;
+    using Football.Core.Entities;
+    using Football.IO;
+    using Football.IO.Contracts;
+    using Football.Models.Players.Contracts;
+    using Football.Models.Players.Entities;
+
     public class StartUp
     {
         public static void Main()
@@ -23,14 +18,7 @@ namespace Football
             Engine engine = new Engine(controller, reader, writer);
             engine.Run();
 
-            
             IPlayer player = new Player();
-
-            
-            //db.BaseClass.ToList();
-            //var team = 
-
         }
-
     }
 }
