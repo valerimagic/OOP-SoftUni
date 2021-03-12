abaf2025c9d64411bd5f62c93c111187596a9145
Valeri Hristov <valerimagic@gmail.com>
2021-03-12 21:23:51 +0200

    update

--- MyProjects/Dataaccess/Entity/FootballDbContext.cs
+++ MyProjects/Dataaccess/Entity/FootballDbContext.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Dataaccess.Entity;
-
-namespace Dataaccess.Entity
+﻿namespace Dataaccess.Entity
 {
+    using Microsoft.EntityFrameworkCore;
+
     public class FootballDbContext : DbContext
     {
-        public DbSet<Teams> Teams { get; set; } 
+        public DbSet<Teams> Teams { get; set; }
 
-        public DbSet<Players> Players { get; set; } 
+        public DbSet<Players> Players { get; set; }
 
-        public DbSet<Statistics> Statistics { get; set; } 
-
+        public DbSet<Statistics> Statistics { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -22,17 +16,8 @@ namespace Dataaccess.Entity
             {
                 optionsBuilder.UseSqlServer(DataSettings.DefaultConnection);
             }
+
             base.OnConfiguring(optionsBuilder);
         }
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder
-        //        .Entity<Teams>()
-        //        .HasMany(p => p.Players)
-        //        .WithOne(t => t.Teams)
-        //        .HasForeignKey(t => t.TeamId)
-        //        .OnDelete(DeleteBehavior.Restrict);
-        //}
     }
 }
