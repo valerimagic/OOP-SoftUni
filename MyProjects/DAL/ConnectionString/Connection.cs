a5719cc830c8c33f7201b8721176a5ea78dfbad6
Valeri Hristov <valerimagic@gmail.com>
2021-03-13 22:55:54 +0200

    new

--- MyProjects/DAL/ConnectionString/Connection.cs
+++ MyProjects/DAL/ConnectionString/Connection.cs
@@ -6,7 +6,7 @@ namespace DAL.ConnectionString
 {
     public class Connection
     {
-        public const string DefaultConnection =
+        internal const string DefaultConnection =
             "Server=DESKTOP-Q9B2B1D\\SQLEXPRESS;Database=PhonBook;Integrated Security=True;";
     }
 }
