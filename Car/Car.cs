f21b951029f2619a5dcacb73ee2a5a78ce548ed7
Valeri Hristov <valerimagic@gmail.com>
2021-03-23 22:06:48 +0200

    new

---
+++ Car/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarParking
+{
+    public class Car
+    {
+        public Car(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
