c4a209d7d24660ee7cee36353a1b2718123b924e
Valeri Hristov <valerimagic@gmail.com>
2021-03-25 22:48:10 +0200

    new

---
+++ Car/Parking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarParking
+{
+    public class Parking
+    {
+        private List<Car> cars;
+        public Parking()
+        {
+            cars = new List<Car>();
+        }
+
+        //public List<Car> Cars { get; set; }
+        public void AddCar(Car car)
+        {
+            if (this.cars.Count < 10)
+            {
+                cars.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Parking is full");
+            }
+        }
+
+        public IReadOnlyCollection<Car> Cars => this.cars;
+
+    }
+}
