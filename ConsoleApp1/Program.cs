e2d2bdff0f3aaa941072a344848fe4e404ca190e
Valeri Hristov <valerimagic@gmail.com>
2021-03-26 23:37:49 +0200

    new

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = sum(val1, val2);
+        Console.WriteLine(sum);
+        }
+    }
+}
