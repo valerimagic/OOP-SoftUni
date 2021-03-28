0235546b02b6bae078873672e518656a466ade4b
Valeri Hristov <valerimagic@gmail.com>
2021-03-28 22:36:20 +0300

    new

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            for (int i = 0;  i < 100; i++)
+            {
+                a++;
+            }
+                //Console.WriteLine(i);
+        }
+    }
+}
