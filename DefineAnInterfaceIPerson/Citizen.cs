352633723f1dcdeb0f8aa8ca258024ca2eb4ffd4
Valeri Hristov <valerimagic@gmail.com>
2021-03-30 23:45:22 +0300

    new

---
+++ DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string Id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = Id;
+            this.Birthdate = birthdate;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
