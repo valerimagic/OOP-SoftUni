b5733b719e7a41e7d8ca19406efd8df7ec8e4d23
Valeri Hristov <valerimagic@gmail.com>
2021-03-30 23:45:55 +0300

    new

---
+++ DefineAnInterfaceIPerson/IPerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public interface IPerson
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+
+
+    }
+}
