f427b3ef455b272fa4d2bd0a3f661004695654e6
Valeri Hristov <valerimagic@gmail.com>
2021-04-06 22:00:24 +0300

    new

---
+++ Exam/OnlineShop 16aug2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Components/Motherboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineShop.Models.Products.Components;
+using OnlineShop.Common.Enums;
+
+namespace OnlineShop.Models.Products
+{
+    public class Motherboard : Component
+    {
+        public Motherboard(int id, string manufacturer, string model, decimal price, double overallPerformance, int generation) 
+            : base(id, manufacturer, model, price, overallPerformance*1.25, generation)
+        {
+        }
+    }
+}
