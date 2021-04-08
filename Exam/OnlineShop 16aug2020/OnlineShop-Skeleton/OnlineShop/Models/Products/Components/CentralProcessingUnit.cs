5d8edebd88fecd4c86d58a3887750eb831d96073
Valeri Hristov <valerimagic@gmail.com>
2021-04-08 22:40:45 +0300

    new

---
+++ Exam/OnlineShop 16aug2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Components/CentralProcessingUnit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineShop.Models.Products.Components;
+using OnlineShop.Common.Enums;
+
+namespace OnlineShop.Models.Products
+{
+    public class CentralProcessingUnit : Component
+    {
+        public CentralProcessingUnit(int id, string manufacturer, string model, decimal price, double overallPerformance, int generation) 
+            : base(id, manufacturer, model, price, overallPerformance*1.25, generation)
+        {
+        }
+
+        
+    }
+}
