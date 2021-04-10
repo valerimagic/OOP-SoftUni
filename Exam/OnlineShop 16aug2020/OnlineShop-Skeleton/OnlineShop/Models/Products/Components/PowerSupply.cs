afb5839dc7671dcb9a85310efd46caffe433b2f0
Valeri Hristov <valerimagic@gmail.com>
2021-04-10 23:01:24 +0300

    new

---
+++ Exam/OnlineShop 16aug2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Components/PowerSupply.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineShop.Common.Enums;
+using OnlineShop.Models.Products.Components;
+
+namespace OnlineShop.Models.Products
+{
+    public class PowerSupply : Component
+    {
+        public PowerSupply(int id, string manufacturer, string model, decimal price, double overallPerformance, int generation) 
+            : base(id, manufacturer, model, price, overallPerformance*1.05, generation)
+        {
+        }
+    }
+}
