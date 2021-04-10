60fab0260dad6b3c831ad3c2b1b7ff1cd98c9e93
Valeri Hristov <valerimagic@gmail.com>
2021-04-10 22:51:47 +0300

    new

---
+++ Exam/Robot 16apr2020/RobotService/Models/Procedures/Polish.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RobotService.Models.Robots.Contracts;
+
+namespace RobotService.Models.Procedures
+{
+    public class Polish : Procedure
+    {
+        public override void DoService(IRobot robot, int procedureTime)
+        {
+            base.DoService(robot, procedureTime);
+
+            robot.Happiness -= 7;
+            this.Robots.Add(robot);
+        }
+    }
+}
---
+++ Exam/Robot 16apr2020/RobotService/Models/Procedures/Procedure.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RobotService.Models.Procedures.Contracts;
+using RobotService.Models.Robots.Contracts;
+using RobotService.Utilities.Messages;
+
+namespace RobotService.Models.Procedures
+{
+    public abstract class Procedure : IProcedure
+    {
+        private IList<IRobot> robots;
+        protected Procedure()
+        {
+            this.robots = new List<IRobot>();
+        }
+
+        public virtual IList<IRobot> Robots => this.robots;
+
+        public string History()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{GetType().Name}");
+
+            foreach (var VARIABLE in this.robots)
+            {
+                sb.AppendLine(VARIABLE.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public virtual void DoService(IRobot robot, int procedureTime)
+        {
+            if (robot.ProcedureTime < procedureTime)
+            {
+                throw new ArgumentException(ExceptionMessages.InsufficientProcedureTime);
+            }
+
+            robot.ProcedureTime -= procedureTime;
+
+        }
+
+    }
+}
