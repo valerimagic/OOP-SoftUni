5e3414e7b3a8340fbeb09470188694dec2ea9327
Valeri Hristov <valerimagic@gmail.com>
2021-04-10 23:02:56 +0300

    new

---
+++ Exam/OnlineShop 16aug2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Components/Component.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineShop.Common.Enums;
+
+namespace OnlineShop.Models.Products.Components
+{
+    public abstract class Component : Product, IComponent
+    {
+        
+        protected Component(int id, string manufacturer, string model, decimal price, double overallPerformance, int generation) 
+            : base(id, manufacturer, model, price, overallPerformance)
+        {
+            this.Generation = generation;
+        }
+
+        public int Generation  { get; private set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" Generation: {this.Generation}";
+        }
+    }
+}
