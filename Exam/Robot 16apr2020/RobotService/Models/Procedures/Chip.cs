07d029da0119ad712928b85234468145790917ec
Valeri Hristov <valerimagic@gmail.com>
2021-04-20 22:11:39 +0300

    new

---
+++ Exam/Robot 16apr2020/RobotService/Models/Procedures/Chip.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RobotService.Models.Robots.Contracts;
+using RobotService.Utilities.Messages;
+
+namespace RobotService.Models.Procedures
+{
+    public class Chip : Procedure
+    {
+        public override void DoService(IRobot robot, int procedureTime)
+        {
+            base.DoService(robot, procedureTime);
+
+            if (robot.IsChipped)
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.AlreadyChipped, GetType().Name));
+            }
+
+            robot.Happiness -= 5;
+            robot.IsChipped = true;
+            this.Robots.Add(robot);
+        }
+    }
+}
