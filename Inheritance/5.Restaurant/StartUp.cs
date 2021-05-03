fabbaceeaffb81d0d2bf0fc95c6f3fe596684ef9
Valeri Hristov <valerimagic@gmail.com>
2021-05-03 23:35:24 +0300

    new

---
+++ Inheritance/5.Restaurant/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.Reflection;
+//using NUnit.Framework;
+using Restaurant;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            Cake cake = new Cake("Valeri");
+               
+        }
+    }
+}
+
+
+
