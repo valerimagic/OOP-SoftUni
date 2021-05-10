b3ce0d8b7aa08fc3d1e4b43761788887f2e19e89
Valeri Hristov <valerimagic@gmail.com>
2021-05-10 23:03:57 +0300

    new

---
+++ Inheritance/Animals/Animal.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal : ISoundProducable
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        private const string exeptionMessages = "Invalid input!";
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(exeptionMessages);
+                }
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(exeptionMessages);
+                }
+                this.age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return this.gender;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(exeptionMessages);
+                }
+                this.gender = value;
+            }
+        }
+
+        public virtual string ProduceSound()
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(this.GetType().Name)
+                .AppendLine($"{this.Name} {this.age} {this.gender}")
+                .Append(this.ProduceSound());
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
