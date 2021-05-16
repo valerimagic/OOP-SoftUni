cb76ad3afa773a00efb70e543a1b6cb906050ad2
Valeri Hristov <valerimagic@gmail.com>
2021-05-16 21:45:25 +0300

    new

---
+++ Inheritance/NeedForSpeed/SportCar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class SportCar : Car
+    {
+        private const double DefaultFuelConsumption = 10.0;
+
+        public SportCar(int horsePower, double fuel) 
+            : base(horsePower, fuel)
+        {
+        }
+
+        public override double FuelConsumption()
+        {
+            return 5.0;
+
+        }
+    }
+}
