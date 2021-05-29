af24d23e56d62c5d74df3508f5de5a44f9fc37f4
Valeri Hristov <valerimagic@gmail.com>
2021-05-29 22:52:55 +0300

    new

---
+++ Inheritance/Animals/Tomcat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Tomcat : Cat
+    {
+        //public string gender = "Male";
+        public Tomcat(string name, int age) : base(name, age, "Male")
+        {
+            //gender = "Male";
+        }
+
+        public override string ProduceSound()
+        { 
+            return "MEOW";
+        }
+    }
+}
