f7e0b24469173aa05976fee6b9d47d64fbe6c3d0
Valeri Hristov <valerimagic@gmail.com>
2021-05-31 23:46:10 +0300

    new

---
+++ Inheritance/PlayersAndMonsters/Wizard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Wizard : Hero
+    {
+        public Wizard(string username, int level) 
+            : base(username, level)
+        {
+
+        }
+
+        public string Username { get; set; }
+
+        public int Level { get; set; }
+
+    }
+}
