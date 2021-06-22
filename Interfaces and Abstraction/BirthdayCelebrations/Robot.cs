87ab7e98bdbfc380de3996d128633f27915d35a6
Valeri Hristov <valerimagic@gmail.com>
2021-06-22 22:54:31 +0300

    update

---
+++ Interfaces and Abstraction/BirthdayCelebrations/Robot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodShortage
+{
+    public class Robot : IId
+    {
+        public Robot(string model, string id)
+        {
+            this.Model = model;
+            this.Id = id;
+        }
+
+        public string Model { get; private set; }
+        public string Id { get; private set; }
+    }
+}
