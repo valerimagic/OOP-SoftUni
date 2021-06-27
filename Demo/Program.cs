a6f1fea52bfe90d58a12f0acd36203b155595168
Valeri Hristov <valerimagic@gmail.com>
2021-06-27 23:59:44 +0300

    update

---
+++ Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            factoriel(input);
+        }
+
+        static int factoriel(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                int result = n * factoriel(n - 1);
+                Console.WriteLine(result);
+                return result;
+            }
+        }
+    }
+}
