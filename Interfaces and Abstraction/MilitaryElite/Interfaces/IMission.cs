ee9d6e34b14f4dbfed867561d9f7bba9799c04ab
Valeri Hristov <valerimagic@gmail.com>
2021-06-30 23:46:37 +0300

    update

---
+++ Interfaces and Abstraction/MilitaryElite/Interfaces/IMission.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Interfaces
+{
+    public interface IMission
+    {
+        string CodeName { get; }
+
+        string State { get; }
+    }
+}
