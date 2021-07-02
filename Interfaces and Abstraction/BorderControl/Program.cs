d4a4dccb84bece890e692f1fb83d4a28b9862b3f
Valeri Hristov <valerimagic@gmail.com>
2021-07-02 23:22:45 +0300

    update

---
+++ Interfaces and Abstraction/BorderControl/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace BorderControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IIdentifiabee> identifiabees = new List<IIdentifiabee>();
+
+            string input = Console.ReadLine();
+
+            while(input != "End")
+            {
+                var passingerInfo = input.Split();
+                if(passingerInfo.Length == 3)
+                {
+                    identifiabees.Add(new Citizen(passingerInfo[0], int.Parse(passingerInfo[1]), passingerInfo[2]));
+                }
+                else
+                {
+                    identifiabees.Add(new Robot(passingerInfo[0], passingerInfo[1]));
+                }
+
+
+
+
+                input = Console.ReadLine();
+            }
+
+            string lastDigit = Console.ReadLine();
+
+            identifiabees.Where(c=>c.Id.EndsWith(lastDigit))
+                .ToList()
+                .ForEach(p=>Console.WriteLine(p.Id));
+        }
+    }
+}
---
+++ Interfaces and Abstraction/BorderControl/Robot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Robot : IIdentifiabee
+    {
+        private string model;
+        public Robot(string model, string id)
+        {
+            this.model = model;
+            this.Id = id;
+        }
+
+        
+
+        public string Id { get; private set; }
+
+        
+    }
+}
