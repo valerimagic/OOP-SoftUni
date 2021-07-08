9cae9addf77e31e631fdf2e3443dd31acb76cd5f
Valeri Hristov <valerimagic@gmail.com>
2021-07-08 23:52:35 +0300

    update

---
+++ Interfaces and Abstraction/BirthdayCelebrations/Citizen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodShortage
+{
+    public class Citizen : IId, IBirthdayable, IBuyer
+    {
+        public Citizen(string name, string age, string id, string birthday)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthday = birthday;
+            this.Food = 0;
+        }
+
+        public string Name { get; private set; }
+
+        public string Age { get; private set; }
+
+        public string Id { get; private set; }
+
+        public string Birthday { get; private set; }
+
+        public int Food { get; private set; }
+
+        public int BuyFood()
+        {
+            return this.Food += 10;
+        }
+
+    }
+
+}
