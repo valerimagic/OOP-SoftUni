aed914f4c295bc482e298cd5ba5c05017dcd4447
Valeri Hristov <valerimagic@gmail.com>
2021-07-16 23:35:54 +0300

    update

---
+++ MyProjects/Dataaccess/Entity/Teams.cs
@@ -0,0 +1,20 @@
+﻿namespace Dataaccess.Entity
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Teams : BaseClass
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string NameCountry { get; set; }
+
+        public Statistics Statistic { get; set; }
+
+        public ICollection<Players> Players { get; set; } = new HashSet<Players>();
+    }
+}
---
+++ MyProjects/Dataaccess/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dataaccess.Entity;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Dataaccess
+{
+    public class GenericRepository<Tentiry>
+        where Tentiry : BaseClass
+    {
+        private FootballDbContext _context;
+        //private DbSet<>
+        public GenericRepository()
+        {
+            _context = new FootballDbContext();
+        }
+        public void GetAll()
+        {
+
+        }
+
+        public void Add()
+        {
+
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Delete()
+        {
+
+        }
+
+        public void Save()
+        {
+            //if ()
+            //{
+                
+            //}
+        }
+
+    }
+}
