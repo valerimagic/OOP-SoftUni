40990f2f9ba99a82ae12a70e5176fc13e21d383b
Valeri Hristov <valerimagic@gmail.com>
2021-07-20 23:52:31 +0300

    update

---
+++ Interfaces and Abstraction/MilitaryElite/Models/Soldier.cs
@@ -0,0 +1,16 @@
+﻿using MilitaryElite.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class Soldier : ISoldier
+    {
+        public string Id => throw new NotImplementedException();
+
+        public string FirstName => throw new NotImplementedException();
+
+        public string LastName => throw new NotImplementedException();
+    }
+}
---
+++ Interfaces and Abstraction/Telephony/ICalling.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telephony
+{
+    public interface ICalling
+    {
+        void Call(string number);
+    }
+}
