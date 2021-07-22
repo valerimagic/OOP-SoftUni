ae3c77c622bd21cacfd50548dc06570d269e7069
Valeri Hristov <valerimagic@gmail.com>
2021-07-22 23:07:34 +0300

    update

---
+++ Interfaces and Abstraction/BorderControl/IIdentifiable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public interface IIdentifiabee
+    {
+        
+
+        string  Id { get; }
+    }
+}
---
+++ Interfaces and Abstraction/MilitaryElite/Interfaces/Soldiers/Private/ILieutenantGeneral.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Interfaces.Soldiers.Private
+{
+    public interface ILieutenantGeneral
+    {
+        //IReadOnlyList<Private> Privates { get; }
+
+    }
+}
