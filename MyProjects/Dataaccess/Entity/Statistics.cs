6eee838a8ff6c44f5f634e107906b4f83e220182
Valeri Hristov <valerimagic@gmail.com>
2021-07-25 22:53:42 +0300

    update

---
+++ MyProjects/Dataaccess/Entity/Statistics.cs
@@ -0,0 +1,13 @@
+﻿namespace Dataaccess.Entity
+{
+    public class Statistics : BaseClass
+    {
+        public int NumberOfPlayedMatches { get; set; }
+
+        public int Win { get; set; }
+
+        public int Loss { get; set; }
+
+        public int Draw { get; set; }
+    }
+}
---
+++ MyProjects/Dataaccess/Migrations/20210224171938_initial_database_structure.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dataaccess.Migrations
+{
+    public partial class initial_database_structure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Statistics",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumberOfPlayedMatches = table.Column<int>(type: "int", nullable: false),
+                    Win = table.Column<int>(type: "int", nullable: false),
+                    Loss = table.Column<int>(type: "int", nullable: false),
+                    Draw = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statistics", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    NameCountry = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    PlayerIDID = table.Column<int>(type: "int", nullable: true),
+                    StatisticIDID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Teams_Players_PlayerIDID",
+                        column: x => x.PlayerIDID,
+                        principalTable: "Players",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Teams_Statistics_StatisticIDID",
+                        column: x => x.StatisticIDID,
+                        principalTable: "Statistics",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_PlayerIDID",
+                table: "Teams",
+                column: "PlayerIDID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_StatisticIDID",
+                table: "Teams",
+                column: "StatisticIDID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teams");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Statistics");
+        }
+    }
+}
