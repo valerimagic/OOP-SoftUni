493bb9f0ec0394358cd4de71cdbe9e1bc941b3a4
Valeri Hristov <valerimagic@gmail.com>
2021-08-03 23:23:41 +0300

    update

---
+++ Interfaces and Abstraction/MilitaryElite/Models/Mission.cs
@@ -0,0 +1,14 @@
+﻿using MilitaryElite.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class Mission : IMission
+    {
+        public string CodeName => throw new NotImplementedException();
+
+        public string State => throw new NotImplementedException();
+    }
+}
