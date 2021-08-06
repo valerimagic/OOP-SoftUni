6275cacab686bbf89a4db75fdf8982bfc0c2edb1
Valeri Hristov <valerimagic@gmail.com>
2021-08-06 23:34:23 +0300

    update

---
+++ MyProjects/Encapsulation/Problem 5. Pizza Calories/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            string inp = input[0];
+            string doughType = input[1];
+            string tehnique = input[2];
+            double doughGrams = double.Parse(input[3]);
+
+            try
+            {
+                Dough dough = new Dough(doughType, tehnique, doughGrams);
+
+                Console.WriteLine(dough.Calculate().ToString("f2"));
+            }
+            catch(ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+           
+
+        }
+    }
+}
---
+++ MyProjects/Encapsulation/Problem 5. Pizza Calories/Topping.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Topping
+    {
+        private double weight;
+        private string toppingType;
+
+        public Topping(double weight, string toppingType)
+        {
+            this.Weight = weight;
+            this.ToppingType = toppingType;
+        }
+
+        public double Weight
+        {
+            get => this.weight;
+            set
+            {
+                if (value < 1 || value > 50)
+                {
+                    throw new ArgumentException($"{this.ToppingType} weight should be in the range[1..50].");
+                }
+
+                this.weight = value;
+            }
+        }
+
+        public string ToppingType
+        {
+            get => this.toppingType;
+            set
+            {
+                if (value != "Meat" && value != "Veggies" && value != "Cheese" && value != "Sause")
+                {
+                    throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+                }
+
+                this.toppingType = value;
+            }
+        }
+
+        public double CalculateTopping(double Weight)
+        {
+            double result = this.Weight * this.ToppingTypeConstant(this.)
+        }
+
+        public double ToppingTypeConstant(string toppingType)
+        {
+            if (toppingType == "Meat")
+            {
+                return 1.2;
+            }
+            else if (toppingType == "Veggies")
+            {
+                return 0.8;
+            }
+            else if (toppingType == "Cheese")
+            {
+                return 1.1;
+            }
+            else if (toppingType == "Sause")
+            {
+                return 0.9;
+            }
+
+            return 2.0;
+        }
+    }
+}
