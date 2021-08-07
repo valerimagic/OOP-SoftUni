d6aa3635940033563f8e57e0567323562ae1b33d
Valeri Hristov <valerimagic@gmail.com>
2021-08-07 23:58:41 +0300

    update

---
+++ MyProjects/Dataaccess/Entity/DataSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Dataaccess.Entity
+{
+    public class DataSettings
+    {
+         internal const string DefaultConnection =
+            "Server=DESKTOP-Q9B2B1D\\SQLEXPRESS;Database=Football;Integrated Security=True;";
+    }
+}
---
+++ MyProjects/Dataaccess/Migrations/20210227125010_AddRelationOneToMany.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dataaccess.Migrations
+{
+    public partial class AddRelationOneToMany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeamId",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_TeamId",
+                table: "Players",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_TeamId",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "Players");
+        }
+    }
+}
