abbf2a25f9f5aa536c00f7feeaf62159e3f13db0
Valeri Hristov <valerimagic@gmail.com>
2021-08-10 23:50:16 +0300

    update

---
+++ MyProjects/Proj/FactoryMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Samsubg model= new Samsubg();
+            model.Asembling();
+
+        }
+    }
+}
---
+++ MyProjects/Proj/FactoryMethod/Samsubg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class Samsubg : Factory
+    {
+        public override void Asembling()
+        {
+            Console.WriteLine("samsubg ");
+        }
+
+        public override void testing()
+        {
+            Console.WriteLine("samsubg testing");
+        }
+
+        public override void Packaging()
+        {
+            Console.WriteLine("samsubg pack");
+        }
+    }
+}
