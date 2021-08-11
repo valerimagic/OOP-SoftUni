3bfceab23451122ee6380c123333c0ae540664b3
Valeri Hristov <valerimagic@gmail.com>
2021-08-11 23:43:21 +0300

    update

---
+++ MyProjects/Proj/Generic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Generic.Models;
+using Generic.Repo;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericFActory<BMW> bmwRepo = new GenericFActory<BMW>();
+            bmwRepo.CableAsembly();
+            GenericFActory<Audi> audiRepo = new GenericFActory<Audi>();
+            audiRepo.CableAsembly();
+
+            Console.ReadKey(true);
+
+
+            { 
+            }
+        }
+    }
+}
