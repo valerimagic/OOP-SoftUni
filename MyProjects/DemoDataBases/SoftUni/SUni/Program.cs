953f58f2f3271bf4f6da0ef67d7d5ce547a6b07e
Valeri Hristov <valerimagic@gmail.com>
2021-08-15 22:58:41 +0300

    update

---
+++ MyProjects/DemoDataBases/SoftUni/SUni/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SUni.Data.Models;
+
+namespace SUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                var department = db.Departments.
+            }
+        }
+    }
+}
