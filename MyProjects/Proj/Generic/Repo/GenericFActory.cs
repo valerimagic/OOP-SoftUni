fbdf82d6f018eee4daaab614e0324a8a89a7eb2d
Valeri Hristov <valerimagic@gmail.com>
2021-08-17 19:06:45 +0300

    update

---
+++ MyProjects/Proj/Generic/Repo/GenericFActory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic.Repo
+{
+    public class GenericFActory <T> where T : new ()
+    {
+        public void CreateBody(T model)
+        {
+            Console.WriteLine(typeof(T) + "is ccreate a body");
+        }
+
+        public void CableAsembly()
+        {
+            Console.WriteLine(typeof(T) + "is cable asemble ");
+        }
+
+        public void Text()
+        {
+            Console.WriteLine(typeof(T) + "is test");
+        }
+    }
+}
