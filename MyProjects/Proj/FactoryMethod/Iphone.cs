c45035a96d227909fa35184b3e2e6069d758b5b5
Valeri Hristov <valerimagic@gmail.com>
2021-08-18 22:45:30 +0300

    update

---
+++ MyProjects/Proj/FactoryMethod/Iphone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class Iphone : Factory
+    {
+        public override void Asembling()
+        {
+            Console.WriteLine("Iphone");
+        }
+
+        public override void testing()
+        {
+            Console.WriteLine("Iphone testing");
+        }
+
+        public override void Packaging()
+        {
+            Console.WriteLine("Iphone pack");
+        }
+    }
+}
