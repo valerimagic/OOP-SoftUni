e08690220bf9c93cb888d58e33b5ad775b9dc04d
Valeri Hristov <valerimagic@gmail.com>
2021-08-19 22:42:31 +0300

    update

---
+++ MyProjects/SalesDatabase/SalesDatabase/Data/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesDatabase.Data.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public double Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ICollection<Sale> Sales { get; set; }
+
+    }
+}
---
+++ MyProjects/SalesDatabase/SalesDatabase/Data/Models/Store.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesDatabase.Data.Models
+{
+    public class Store
+    {
+        public int StoreId { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Sale> Sales { get; set; }
+    }
+}
