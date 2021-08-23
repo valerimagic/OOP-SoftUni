85e51781be3711ea3bcbe603f0cbe4e123e07401
Valeri Hristov <valerimagic@gmail.com>
2021-08-23 23:27:42 +0300

    update

---
+++ Restaurant/HotBeverage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class HotBeverage : Beverages
+    {
+        public HotBeverage(string name, decimal price, double milliliter) : base(name, price, milliliter)
+        {
+
+
+        }
+    }
+}
