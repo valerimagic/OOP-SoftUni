955080c56c097d95691364514ca4c625572ce464
Valeri Hristov <valerimagic@gmail.com>
2021-08-24 13:27:32 +0300

    update

---
+++ MyProjects/SalesDatabase/SalesDatabase/Data/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesDatabase.Data.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string CreditCardNumber { get; set; }
+
+        public ICollection<Sale> Sales { get; set; }
+
+    }
+}
---
+++ MyProjects/SalesDatabase/SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesDatabase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext()
+        {
+            
+        }
+
+        public SalesContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
