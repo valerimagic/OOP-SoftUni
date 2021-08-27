6f94f53932b80a19c4217b2890d7575d6a638251
Valeri Hristov <valerimagic@gmail.com>
2021-08-27 23:34:57 +0300

    update

---
+++ MyProjects/Proj/FactoryMethod/Factory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace FactoryMethod
+{
+    abstract class Factory
+    {
+        public abstract void Asembling();
+        public abstract void testing();
+        public abstract void Packaging();
+    }
+}
