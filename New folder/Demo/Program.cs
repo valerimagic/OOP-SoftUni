9ca62254d6ad2330b2e9a0ac43395d8c1ac70ef6
Valeri Hristov <valerimagic@gmail.com>
2021-08-30 23:49:49 +0300

    update

---
+++ New folder/Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                int value = i;
+            }
+        }
+    }
+}
---
+++ Raiding/BaseHero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public abstract class BaseHero
+    {
+        //private string name;
+        //private int power;
+
+        public BaseHero(string name, int power)
+        {
+            this.Name = name;
+            this.Power = power;
+        }
+
+        public string Name { get; set; }
+        public int Power { get; set; }
+
+
+
+
+        public virtual string CastAbility()
+        {
+            return $"{GetType().Name} – {this.Name} healed for {this.Power}";
+        }
+
+
+
+
+    }
+}
