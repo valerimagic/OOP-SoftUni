d36f8dc59c7787a3282f4129c7c79b387d6bdc53
Valeri Hristov <valerimagic@gmail.com>
2021-09-01 23:57:54 +0300

    update

---
+++ Restaurant/Coffee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const decimal CoffeePrice = 3.50m;
+        private const double CoffeeMilliliters = 50;
+        public Coffee(string name, double caffeine) : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            //coffeeMilliliters = 50;
+            this.Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; internal set; }
+    }
+}
