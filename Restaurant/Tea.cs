e9267c441fed841f3e5c4f77d0957aea9887ca75
Valeri Hristov <valerimagic@gmail.com>
2021-09-04 23:14:29 +0300

    update

---
+++ Restaurant/Tea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Tea : Beverages
+    {
+        public Tea(string name, decimal price, double milliliter) : base(name, price, milliliter)
+        {
+        }
+    }
+}
