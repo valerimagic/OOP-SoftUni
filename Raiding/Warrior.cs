7f602bab4429d3de10258aab44445f7b9487d78e
Valeri Hristov <valerimagic@gmail.com>
2021-09-06 23:57:39 +0300

    update

---
+++ Raiding/Warrior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Warrior : BaseHero
+    {
+        private const int powerWarrior = 100;
+        public Warrior(string name) 
+            : base(name, powerWarrior)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{GetType().Name} - {this.Name} hit for {this.Power} damage";
+        }
+    }
+}
