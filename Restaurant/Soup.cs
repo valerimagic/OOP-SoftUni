11e2c5ee315008b434008b7fef87e746a204622e
Valeri Hristov <valerimagic@gmail.com>
2021-09-09 00:22:57 +0300

    update

---
+++ Restaurant/Soup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Soup : MainDish
+    {
+        public Soup(string name, decimal price, double grams) : base(name, price, grams)
+        {
+        }
+    }
+}
