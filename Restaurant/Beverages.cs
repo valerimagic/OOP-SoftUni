7b73b19fc599b1e0b29d64523c7bd06b319d1748
Valeri Hristov <valerimagic@gmail.com>
2021-09-08 23:27:36 +0300

    update

---
+++ Restaurant/Beverages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public abstract class Beverages : Product
+    {
+        private double milliliters;
+        public Beverages(string name, decimal price, double milliliters) 
+            : base(name, price)
+        {
+            this.Milliliters = milliliters;
+        }
+
+        public double Milliliters
+        {
+            get;
+            internal set;
+        }
+    }
+    
+}
