ad4e02c248da8f16fc51a2bf0cc0ce4b63b5ff43
Valeri Hristov <valerimagic@gmail.com>
2021-10-03 23:55:32 +0300

    update

---
+++ VehiclesOne/Bus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehiclesOne
+{
+    public class Bus : Vehicle
+    {
+        //private double tankCapacity;
+        private const double aerCondition = 1.4;
+
+
+
+        public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) : base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+            this.FuelConsumption += aerCondition;
+        }
+
+        public string DriveBusEmpty(double distance)
+        {
+            this.FuelConsumption -= aerCondition;
+            return base.Drive(distance);
+        }
+    }
+}
