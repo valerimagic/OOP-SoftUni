5151d8510feb1bc9f02dff7a955ffe82e4fc1189
Valeri Hristov <valerimagic@gmail.com>
2021-10-13 23:28:37 +0300

    update

---
+++ WildFarm/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public abstract class Animal
+    {
+        private string name;
+        private double weight;
+        //private int foodEaten;
+
+        public Animal(string name, double weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+        }
+
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int FoodEaten { get; set; }
+
+
+        public virtual string ProduceSound()
+        {
+            return "Sound";
+        }
+
+        public virtual void AnimalEat(Food food, double animalCoeficient)
+        {
+            this.FoodEaten += food.Quantity;
+            this.Weight += food.Quantity * animalCoeficient;
+        }
+    }
+}
