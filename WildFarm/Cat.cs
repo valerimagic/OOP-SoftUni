da37244143980556b0f3bbc8e2527ed674df1d14
Valeri Hristov <valerimagic@gmail.com>
2021-10-17 22:42:48 +0300

    update

---
+++ WildFarm/Cat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Cat : Feline
+    {
+        public Cat(string name, double weight, string livingRegion, string breed) 
+            : base(name, weight, livingRegion, breed)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Meow";
+        }
+
+        public override void AnimalEat(Food food, double animalCoeficient)
+        {
+            if (food.GetType().Name != "Vegetable" && food.GetType().Name != "Meat")
+            {
+                throw new ArgumentException($"{this.GetType().Name} does not eat {food.GetType().Name}!");
+            }
+            base.AnimalEat(food, 0.30);
+        }
+    }
+}
