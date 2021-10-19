4031dca949c6603df953bce8d05d8a8709f2a867
Valeri Hristov <valerimagic@gmail.com>
2021-10-19 23:26:29 +0300

    update

---
+++ WildFarm/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Dog : Mammal
+    {
+        public Dog(string name, double weight, string livingRegion) 
+            : base(name, weight, livingRegion)
+        {
+
+        }
+
+        public override string ProduceSound()
+        {
+            return "Woof!";
+        }
+
+        public override void AnimalEat(Food food, double animalCoeficient)
+        {
+            if (food.GetType().Name != "Meat")
+            {
+                throw new ArgumentException($"{this.GetType().Name} does not eat {food.GetType().Name}!");
+            }
+            base.AnimalEat(food, 0.4);
+        }
+    }
+}
