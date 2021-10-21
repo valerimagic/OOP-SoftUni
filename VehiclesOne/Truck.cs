6af1c34966ffeb0a3d97f218970f8f7510ebd074
Valeri Hristov <valerimagic@gmail.com>
2021-10-21 16:16:38 +0300

    update

---
+++ VehiclesOne/Truck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehiclesOne
+{
+    public class Truck : Vehicle
+    {
+        private const double truckSummer = 1.6;
+        private const double tankFuel = 0.05;
+
+        public Truck(double fuelQuantity, double fuelConsumption, double tankCapacity) : base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+            this.FuelConsumption += truckSummer;
+        }
+
+        public override void Refuel(double litres)
+        {
+            base.Refuel(litres);
+            this.FuelQuantity -= litres * tankFuel;
+        }
+    }
+}
